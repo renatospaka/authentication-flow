1c5d40ffb07ea64c7bedba31e437411457029fc0
Renato Spakauskas <renatospaka@gmail.com>
2021-09-09 20:07:08 -0300

    refactor: :art: just minor changes
    update from single quotes to double quotes

--- jwt-auth/src/app.module.ts
+++ jwt-auth/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { LoginController } from "./auth/login/login.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule],
+  controllers: [AppController, LoginController],
   providers: [AppService],
 })
 export class AppModule {}
