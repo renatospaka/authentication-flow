3b1a3c4c78f8d00958819b04f186f39ec7cef53e
Renato Spakauskas <renatospaka@gmail.com>
2021-09-10 17:26:52 -0300

    feat: :construction: jwt hs256
    creation and strategy access for the created token

--- jwt-auth/src/auth/auth.module.ts
+++ jwt-auth/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth/auth.controller";
 import { AuthService } from "./auth/auth.service";
+import { JwtStrategyService } from "./jwt-strategy/jwt-strategy.service";
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: "123456AbCdEf",
+      signOptions: {
+        expiresIn: "60s",
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategyService]
 })
 export class AuthModule {}
--- jwt-auth/src/auth/auth/auth.controller.ts
+++ jwt-auth/src/auth/auth/auth.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post("login")
   login(@Body() body) {
-    this.authService.login(body.username, body.password)
+    return { token: this.authService.login(body.username, body.password)}
+  }
+
+  @Get("/teste")
+  teste() {
+    return {
+      name: "Spakauskas"
+    }
   }
 }
